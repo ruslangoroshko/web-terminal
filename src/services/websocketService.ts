30c17aabd1d62f3e361a1819a494b0bbef196a00
Shadymination <shadi.ganem@simplbit.com>
2020-01-06 17:15:14 +0200

    temp turn off msgpack

--- src/services/websocketService.ts
+++ src/services/websocketService.ts
@@ -4,9 +4,9 @@ import { MessagePackHubProtocol } from '@aspnet/signalr-protocol-msgpack';
 const initConnection = (connectionString: string) => {
   const connection = new signalR.HubConnectionBuilder()
     .withUrl(connectionString)
-    .withHubProtocol(
-      IS_LIVE ? new MessagePackHubProtocol() : new signalR.JsonHubProtocol()
-    )
+    // .withHubProtocol(
+    //   IS_LIVE ? new MessagePackHubProtocol() : new signalR.JsonHubProtocol()
+    // )
     .build();
   return connection;
 };
