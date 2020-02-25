6968d37876d98c1f2183018fe8f6588a68f3a74c
Shadymination <shadi.ganem@simplbit.com>
2020-02-25 15:58:26 +0200

    https://monfex.atlassian.net/browse/WEBT-12

--- src/helpers/getProcessId.ts
+++ src/helpers/getProcessId.ts
@@ -1,8 +1,10 @@
 export const getProcessId = () => {
   // TODO: regex please
-  return new Date()
+  return `${new Date()
     .toISOString()
     .replace('-', '')
     .replace(':', '')
-    .replace('Z', '');
+    .replace('Z', '')}-${Math.random()
+    .toString(36)
+    .substr(2, 9)}`;
 };
