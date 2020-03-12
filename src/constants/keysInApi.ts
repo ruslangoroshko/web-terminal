cd4723ea7af66c720c66492c6051fe0aa0829e77
Shadymination <shadi.ganem@simplbit.com>
2020-03-12 19:00:29 +0200

    removed unnecessary variable

--- src/constants/keysInApi.ts
+++ src/constants/keysInApi.ts
@@ -1,7 +1,6 @@
 const KeysInApi = {
   SELECTED_INSTRUMENTS: 'selected_instruments',
   FAVOURITE_INSTRUMENTS: 'favourite_instruments',
-  FIRST_LOGIN: 'first_login',
   ACTIVE_ACCOUNT_ID: 'aaid',
 };
 
