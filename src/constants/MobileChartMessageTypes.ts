640f95cfc03a03a1ba88c590f1e6429524518967
Shadymination <shadi.ganem@simplbit.com>
2020-03-19 16:02:01 +0200

    mobile interface in progress

---
+++ src/constants/MobileChartMessageTypes.ts
@@ -0,0 +1,10 @@
+export const mobileChartMessageTypes = {
+  SET_CANDLE_TYPE: 'set_candle_type',
+  SET_INTERVAL: 'set_interval',
+  SET_RESOLUTION: 'set_resolution',
+  SET_INSTRUMENT: 'set_instrument',
+};
+
+Object.freeze(mobileChartMessageTypes);
+
+export default mobileChartMessageTypes;
--- src/pages/MobileTradingView.tsx
+++ src/pages/MobileTradingView.tsx
@@ -4,20 +4,108 @@ import Axios from 'axios';
 import initConnection from '../services/websocketService';
 import Topics from '../constants/websocketTopics';
 import { FlexContainer } from '../styles/FlexContainer';
-import MobileChartContainer from '../containers/MobileChartContainer';
+import mobileChartMessageTypes from '../constants/MobileChartMessageTypes';
+import {
+  LanguageCode,
+  ChartingLibraryWidgetOptions,
+  SeriesStyle,
+  widget,
+  IChartingLibraryWidget,
+} from '../vendor/charting_library/charting_library.min';
+import DataFeedService from '../services/dataFeedService';
+import {
+  supportedResolutions,
+  SupportedResolutionsType,
+  supportedInterval,
+} from '../constants/supportedTimeScales';
+import { BASIC_RESOLUTION_KEY } from '../constants/chartValues';
+import { LineStyles } from '../enums/TradingViewStyles';
+import moment from 'moment';
+
+function getLanguageFromURL(): LanguageCode | null {
+  const regex = new RegExp('[\\?&]lang=([^&#]*)');
+  const results = regex.exec(location.search);
+  return results === null
+    ? null
+    : (decodeURIComponent(results[1].replace(/\+/g, ' ')) as LanguageCode);
+}
+
+const containerId = 'tv_chart_container';
 
 const MobileTradingView: FC = () => {
   const [activeSession, setActiveSession] = useState<HubConnection>();
+  const [tvWidget, setTvWidget] = useState<IChartingLibraryWidget>();
 
-  const authenticate = async () => {
-    const response = await Axios.post(
-      `${API_AUTH_STRING}/api/v1/Trader/Authenticate`,
-      {
-        email: 'shadi.ganem@simplbit.com',
-        password: '123123123',
-      }
-    );
-    return response.data;
+  const initWidget = (activeSession: HubConnection, instrumentId: string) => {
+    const widgetOptions: ChartingLibraryWidgetOptions = {
+      symbol: instrumentId,
+      datafeed: new DataFeedService(activeSession, instrumentId),
+      interval: supportedResolutions[BASIC_RESOLUTION_KEY],
+      container_id: containerId,
+      library_path: CHARTING_LIBRARY_PATH,
+      locale: getLanguageFromURL() || 'en',
+      custom_css_url: 'custom_trading_view_styles.css',
+      disabled_features: [
+        'header_widget',
+        'timeframes_toolbar',
+        'use_localstorage_for_settings',
+        'border_around_the_chart',
+        'left_toolbar',
+        'symbol_info',
+        'context_menus',
+        'main_series_scale_menu',
+      ],
+      enabled_features: ['remove_library_container_border'],
+      fullscreen: false,
+      autosize: true,
+      overrides: {
+        'symbolWatermarkProperties.transparency': 90,
+        'mainSeriesProperties.style': SeriesStyle.Area,
+        'mainSeriesProperties.lineStyle.color': '#21B3A4',
+        'mainSeriesProperties.lineStyle.linestyle': LineStyles.LINESTYLE_SOLID,
+        'mainSeriesProperties.lineStyle.linewidth': 3,
+        'mainSeriesProperties.lineStyle.priceSource': 'close',
+        'mainSeriesProperties.areaStyle.color1': 'rgba(0, 255, 221, 0.08)',
+        'mainSeriesProperties.areaStyle.color2': 'rgba(0, 255, 221, 0.08)',
+        'mainSeriesProperties.areaStyle.linecolor': '#21B3A4',
+        'mainSeriesProperties.areaStyle.linestyle': LineStyles.LINESTYLE_SOLID,
+        'mainSeriesProperties.areaStyle.linewidth': 3,
+        'mainSeriesProperties.candleStyle.upColor': '#21B3A4',
+        'mainSeriesProperties.candleStyle.downColor': '#21B3A4',
+        'mainSeriesProperties.candleStyle.drawWick': true,
+        'mainSeriesProperties.candleStyle.drawBorder': false,
+        'mainSeriesProperties.candleStyle.borderColor': '#28555a',
+        'mainSeriesProperties.candleStyle.borderUpColor': '#21B3A4',
+        'mainSeriesProperties.candleStyle.borderDownColor': '#ed145b',
+        'mainSeriesProperties.candleStyle.wickUpColor': '#255258',
+        'mainSeriesProperties.candleStyle.wickDownColor': '#622243',
+        'mainSeriesProperties.candleStyle.barColorsOnPrevClose': false,
+        'mainSeriesProperties.areaStyle.priceSource': 'close',
+        'paneProperties.axisProperties.autoScale': false,
+        'paneProperties.vertGridProperties.color': 'rgba(255, 255, 255, 0.08)',
+        'paneProperties.vertGridProperties.style': LineStyles.LINESTYLE_DOTTED,
+        'paneProperties.horzGridProperties.color': 'rgba(255, 255, 255, 0.08)',
+        'paneProperties.horzGridProperties.style': LineStyles.LINESTYLE_DOTTED,
+        'paneProperties.legendProperties.showStudyArguments': false,
+        'paneProperties.legendProperties.showStudyTitles': false,
+        'paneProperties.legendProperties.showStudyValues': false,
+        'paneProperties.legendProperties.showSeriesTitle': false,
+        'paneProperties.legendProperties.showSeriesOHLC': true,
+        'paneProperties.legendProperties.showLegend': false,
+        'paneProperties.legendProperties.showBarChange': false,
+        'paneProperties.legendProperties.showOnlyPriceSource': false,
+        'linetoolnote.backgroundColor': '#ed145b',
+        'scalesProperties.lineColor': 'transparent',
+        'scalesProperties.textColor': 'rgba(255, 255, 255, 0.2)',
+        'scalesProperties.backgroundColor': 'transparent',
+        'paneProperties.background': 'rgba(0,0,0,0)',
+        'mainSeriesProperties.priceLineColor': '#fff',
+        'mainSeriesProperties.priceLineWidth': 2,
+        'timeScale.rightOffset': 5,
+      },
+    };
+
+    setTvWidget(new widget(widgetOptions));
   };
 
   const initWebsocketConnection = async (token: string) => {
@@ -29,21 +117,110 @@ const MobileTradingView: FC = () => {
     } catch (error) {}
   };
 
+  const setInterval = (newInterval: string) => {
+    let from = moment();
+    let newResolutionKey: SupportedResolutionsType = '1 minute';
+    switch (newInterval) {
+      case supportedInterval['1D']:
+        from = moment().subtract(1, 'd');
+        newResolutionKey = '1 minute';
+        break;
+
+      case supportedInterval['5D']:
+        from = moment().subtract(5, 'd');
+        newResolutionKey = '30 minutes';
+        break;
+
+      case supportedInterval['1M']:
+        from = moment().subtract(1, 'M');
+        newResolutionKey = '1 hour';
+        break;
+
+      case supportedInterval['YTD']:
+        from = moment().subtract(new Date().getUTCMonth(), 'M');
+        newResolutionKey = '1 day';
+        break;
+
+      case supportedInterval['1Y']:
+        from = moment().subtract(1, 'year');
+        newResolutionKey = '1 day';
+        break;
+
+      case supportedInterval['3Y']:
+        from = moment().subtract(1, 'y');
+        newResolutionKey = '1 day';
+        break;
+
+      case supportedInterval['All']:
+        from = moment().subtract(1, 'y');
+        newResolutionKey = '1 day';
+        break;
+
+      default:
+        break;
+    }
+
+    tvWidget
+      ?.chart()
+      .setResolution(supportedResolutions[newResolutionKey], () => {
+        tvWidget?.chart().setVisibleRange({
+          from: from.valueOf(),
+          to: moment().valueOf(),
+        });
+      });
+  };
+
+  const messageHandler = (e: MessageEvent) => {
+    if (!activeSession) {
+      Axios.defaults.headers['Authorization'] = e.data.token;
+      initWebsocketConnection(e.data.token).then(() => {
+        initWidget(activeSession!, e.data.instrument);
+      });
+    }
+
+    if (e.data.type) {
+      switch (e.data.type) {
+        case mobileChartMessageTypes.SET_CANDLE_TYPE:
+          tvWidget?.chart().setChartType(e.data.message);
+          break;
+
+        case mobileChartMessageTypes.SET_INSTRUMENT:
+          tvWidget?.setSymbol(e.data.instrument, e.data.interval, () => {});
+          break;
+
+        case mobileChartMessageTypes.SET_INTERVAL:
+          setInterval(e.data);
+          break;
+
+        case mobileChartMessageTypes.SET_RESOLUTION:
+          tvWidget?.chart().setResolution(e.data.resolution, () => {});
+          break;
+
+        default:
+          break;
+      }
+    }
+  };
+
   useEffect(() => {
-    authenticate().then(({ data }) => {
-      Axios.defaults.headers['Authorization'] = data.token;
-      initWebsocketConnection(data.token);
-    });
+    window.addEventListener('message', messageHandler);
+    return window.removeEventListener('message', messageHandler);
   }, []);
+
+  useEffect(() => {
+    tvWidget?.onChartReady(async () => {
+      //   tradingViewStore.tradingWidget = tvWidget;
+    });
+    return () => {
+      tvWidget?.remove();
+    };
+  }, [tvWidget]);
 
   return (
     <FlexContainer height="100vh" width="100vw">
       <FlexContainer width="100%">
         {activeSession && (
-          <MobileChartContainer
-            activeSession={activeSession}
-            instrumentId="EURUSD"
-          ></MobileChartContainer>
+          <FlexContainer width="100%" height="100%" id={containerId} />
         )}
       </FlexContainer>
     </FlexContainer>
