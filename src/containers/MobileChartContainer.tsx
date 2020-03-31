cab5bbe39bede5c3b0610779a79b3974af65a300
Shadymination <shadi.ganem@simplbit.com>
2020-03-31 17:34:36 +0300

    mobile tradingview tries #11

---
+++ src/containers/MobileChartContainer.tsx
@@ -0,0 +1,118 @@
+import React, { FC, useEffect } from 'react';
+import {
+  ChartingLibraryWidgetOptions,
+  LanguageCode,
+  widget,
+  SeriesStyle,
+  IChartingLibraryWidget,
+} from '../vendor/charting_library/charting_library.min';
+import { FlexContainer } from '../styles/FlexContainer';
+import DataFeedService from '../services/dataFeedService';
+import { LineStyles } from '../enums/TradingViewStyles';
+import ColorsPallete from '../styles/colorPallete';
+import { supportedResolutions } from '../constants/supportedTimeScales';
+import { BASIC_RESOLUTION_KEY } from '../constants/chartValues';
+import { observer } from 'mobx-react-lite';
+import { HubConnection } from '@aspnet/signalr';
+
+function getLanguageFromURL(): LanguageCode | null {
+  const regex = new RegExp('[\\?&]lang=([^&#]*)');
+  const results = regex.exec(location.search);
+  return results === null
+    ? null
+    : (decodeURIComponent(results[1].replace(/\+/g, ' ')) as LanguageCode);
+}
+
+const containerId = 'tv_chart_container';
+
+interface IProps {
+  instrumentId: string;
+  activeSession: HubConnection;
+  callbackWidget: (widget: IChartingLibraryWidget) => void;
+}
+
+const MobileChartContainer: FC<IProps> = observer(({ instrumentId, activeSession, callbackWidget }) => {
+  useEffect(() => {
+    const widgetOptions: ChartingLibraryWidgetOptions = {
+      symbol: instrumentId,
+      datafeed: new DataFeedService(activeSession, instrumentId),
+      interval: supportedResolutions[BASIC_RESOLUTION_KEY],
+      container_id: containerId,
+      library_path: CHARTING_LIBRARY_PATH,
+      locale: getLanguageFromURL() || 'en',
+      custom_css_url: 'custom_trading_view_styles.css',
+      disabled_features: [
+        'header_widget',
+        // 'legend_widget',
+        'timeframes_toolbar',
+        'use_localstorage_for_settings',
+        'border_around_the_chart',
+        'left_toolbar',
+        'symbol_info',
+        'context_menus',
+        'main_series_scale_menu',
+      ],
+      enabled_features: ['remove_library_container_border'],
+      fullscreen: false,
+      autosize: true,
+      overrides: {
+        'symbolWatermarkProperties.transparency': 90,
+        'mainSeriesProperties.style': SeriesStyle.Area,
+        'mainSeriesProperties.lineStyle.color': ColorsPallete.MINT,
+        'mainSeriesProperties.lineStyle.linestyle': LineStyles.LINESTYLE_SOLID,
+        'mainSeriesProperties.lineStyle.linewidth': 3,
+        'mainSeriesProperties.lineStyle.priceSource': 'close',
+        'mainSeriesProperties.areaStyle.color1': 'rgba(0, 255, 221, 0.08)',
+        'mainSeriesProperties.areaStyle.color2': 'rgba(0, 255, 221, 0.08)',
+        'mainSeriesProperties.areaStyle.linecolor': ColorsPallete.MINT,
+        'mainSeriesProperties.areaStyle.linestyle': LineStyles.LINESTYLE_SOLID,
+        'mainSeriesProperties.areaStyle.linewidth': 3,
+        'mainSeriesProperties.candleStyle.upColor': '#21B3A4',
+        'mainSeriesProperties.candleStyle.downColor': '#ed145b',
+        'mainSeriesProperties.candleStyle.drawWick': true,
+        'mainSeriesProperties.candleStyle.drawBorder': false,
+        'mainSeriesProperties.candleStyle.borderColor': '#28555a',
+        'mainSeriesProperties.candleStyle.borderUpColor': '#21B3A4',
+        'mainSeriesProperties.candleStyle.borderDownColor': '#ed145b',
+        'mainSeriesProperties.candleStyle.wickUpColor': '#255258',
+        'mainSeriesProperties.candleStyle.wickDownColor': '#622243',
+        'mainSeriesProperties.candleStyle.barColorsOnPrevClose': false,
+        'mainSeriesProperties.areaStyle.priceSource': 'close',
+        'paneProperties.axisProperties.autoScale': false,
+        'paneProperties.vertGridProperties.color': 'rgba(255, 255, 255, 0.08)',
+        'paneProperties.vertGridProperties.style': LineStyles.LINESTYLE_DOTTED,
+        'paneProperties.horzGridProperties.color': 'rgba(255, 255, 255, 0.08)',
+        'paneProperties.horzGridProperties.style': LineStyles.LINESTYLE_DOTTED,
+        'paneProperties.legendProperties.showStudyArguments': false,
+        'paneProperties.legendProperties.showStudyTitles': false,
+        'paneProperties.legendProperties.showStudyValues': false,
+        'paneProperties.legendProperties.showSeriesTitle': false,
+        'paneProperties.legendProperties.showSeriesOHLC': true,
+        'paneProperties.legendProperties.showLegend': false,
+        'paneProperties.legendProperties.showBarChange': false,
+        'paneProperties.legendProperties.showOnlyPriceSource': false,
+        'linetoolnote.backgroundColor': ColorsPallete.RAZZMATAZZ,
+        'scalesProperties.lineColor': 'transparent',
+        'scalesProperties.textColor': 'rgba(255, 255, 255, 0.2)',
+        'scalesProperties.backgroundColor': 'transparent',
+        'paneProperties.background': 'rgba(0,0,0,0)',
+        'mainSeriesProperties.priceLineColor': '#fff',
+        'mainSeriesProperties.priceLineWidth': 2,
+        'timeScale.rightOffset': 5,
+      },
+    };
+
+    const tvWidget = new widget(widgetOptions);
+
+    tvWidget.onChartReady(async () => {
+      callbackWidget(tvWidget);
+    });
+    return () => {
+      tvWidget.remove();
+    };
+  }, []);
+
+  return <FlexContainer width="100%" height="100%" id={containerId} />;
+});
+
+export default MobileChartContainer;
--- src/pages/MobileTradingView.tsx
+++ src/pages/MobileTradingView.tsx
@@ -6,125 +6,37 @@ import Topics from '../constants/websocketTopics';
 import { FlexContainer } from '../styles/FlexContainer';
 import mobileChartMessageTypes from '../constants/MobileChartMessageTypes';
 import {
-  LanguageCode,
-  ChartingLibraryWidgetOptions,
-  SeriesStyle,
-  widget,
   IChartingLibraryWidget,
 } from '../vendor/charting_library/charting_library.min';
-import DataFeedService from '../services/dataFeedService';
 import {
   supportedResolutions,
   SupportedResolutionsType,
   supportedInterval,
 } from '../constants/supportedTimeScales';
-import { BASIC_RESOLUTION_KEY } from '../constants/chartValues';
-import { LineStyles } from '../enums/TradingViewStyles';
 import moment from 'moment';
-import { PrimaryTextParagraph } from '../styles/TextsElements';
-import { StatusCodesMobileTW } from '../enums/StatusCodesMobileTW';
+import MobileChartContainer from '../containers/MobileChartContainer';
 
-function getLanguageFromURL(): LanguageCode | null {
-  const regex = new RegExp('[\\?&]lang=([^&#]*)');
-  const results = regex.exec(location.search);
-  return results === null
-    ? null
-    : (decodeURIComponent(results[1].replace(/\+/g, ' ')) as LanguageCode);
-}
-
-const containerId = 'tv_chart_container';
 
 const MobileTradingView: FC = () => {
   const [activeSession, setActiveSession] = useState<HubConnection>();
   const [tvWidget, setTvWidget] = useState<IChartingLibraryWidget>();
+  const [instrumentId, setInstrumentId] = useState('');
 
-  function initWidget(activeSession: HubConnection, instrumentId: string) {
-    alert('starting widget init');
-    const widgetOptions: ChartingLibraryWidgetOptions = {
-      symbol: instrumentId,
-      datafeed: new DataFeedService(activeSession, instrumentId),
-      interval: supportedResolutions[BASIC_RESOLUTION_KEY],
-      container_id: containerId,
-      library_path: CHARTING_LIBRARY_PATH,
-      locale: getLanguageFromURL() || 'en',
-      custom_css_url: 'custom_trading_view_styles.css',
-      disabled_features: [
-        'header_widget',
-        'timeframes_toolbar',
-        'use_localstorage_for_settings',
-        'border_around_the_chart',
-        'left_toolbar',
-        'symbol_info',
-        'context_menus',
-        'main_series_scale_menu',
-      ],
-      enabled_features: ['remove_library_container_border'],
-      fullscreen: false,
-      autosize: true,
-      overrides: {
-        'symbolWatermarkProperties.transparency': 90,
-        'mainSeriesProperties.style': SeriesStyle.Area,
-        'mainSeriesProperties.lineStyle.color': '#21B3A4',
-        'mainSeriesProperties.lineStyle.linestyle': LineStyles.LINESTYLE_SOLID,
-        'mainSeriesProperties.lineStyle.linewidth': 3,
-        'mainSeriesProperties.lineStyle.priceSource': 'close',
-        'mainSeriesProperties.areaStyle.color1': 'rgba(0, 255, 221, 0.08)',
-        'mainSeriesProperties.areaStyle.color2': 'rgba(0, 255, 221, 0.08)',
-        'mainSeriesProperties.areaStyle.linecolor': '#21B3A4',
-        'mainSeriesProperties.areaStyle.linestyle': LineStyles.LINESTYLE_SOLID,
-        'mainSeriesProperties.areaStyle.linewidth': 3,
-        'mainSeriesProperties.candleStyle.upColor': '#21B3A4',
-        'mainSeriesProperties.candleStyle.downColor': '#21B3A4',
-        'mainSeriesProperties.candleStyle.drawWick': true,
-        'mainSeriesProperties.candleStyle.drawBorder': false,
-        'mainSeriesProperties.candleStyle.borderColor': '#28555a',
-        'mainSeriesProperties.candleStyle.borderUpColor': '#21B3A4',
-        'mainSeriesProperties.candleStyle.borderDownColor': '#ed145b',
-        'mainSeriesProperties.candleStyle.wickUpColor': '#255258',
-        'mainSeriesProperties.candleStyle.wickDownColor': '#622243',
-        'mainSeriesProperties.candleStyle.barColorsOnPrevClose': false,
-        'mainSeriesProperties.areaStyle.priceSource': 'close',
-        'paneProperties.axisProperties.autoScale': false,
-        'paneProperties.vertGridProperties.color': 'rgba(255, 255, 255, 0.08)',
-        'paneProperties.vertGridProperties.style': LineStyles.LINESTYLE_DOTTED,
-        'paneProperties.horzGridProperties.color': 'rgba(255, 255, 255, 0.08)',
-        'paneProperties.horzGridProperties.style': LineStyles.LINESTYLE_DOTTED,
-        'paneProperties.legendProperties.showStudyArguments': false,
-        'paneProperties.legendProperties.showStudyTitles': false,
-        'paneProperties.legendProperties.showStudyValues': false,
-        'paneProperties.legendProperties.showSeriesTitle': false,
-        'paneProperties.legendProperties.showSeriesOHLC': true,
-        'paneProperties.legendProperties.showLegend': false,
-        'paneProperties.legendProperties.showBarChange': false,
-        'paneProperties.legendProperties.showOnlyPriceSource': false,
-        'linetoolnote.backgroundColor': '#ed145b',
-        'scalesProperties.lineColor': 'transparent',
-        'scalesProperties.textColor': 'rgba(255, 255, 255, 0.2)',
-        'scalesProperties.backgroundColor': 'transparent',
-        'paneProperties.background': 'rgba(0,0,0,0)',
-        'mainSeriesProperties.priceLineColor': '#fff',
-        'mainSeriesProperties.priceLineWidth': 2,
-        'timeScale.rightOffset': 5,
-      },
-    };
-
-    setTvWidget(new widget(widgetOptions));
-    alert('setTvWidget');
-  };
-
-  const initWebsocketConnection = async (token: string) => {
+  const initWebsocketConnection = async (
+    token: string,
+    instrumentId: string
+  ) => {
     const connection = initConnection(WS_HOST);
     try {
       await connection.start();
+      setInstrumentId(instrumentId);
       setActiveSession(connection);
       try {
         alert('ws connection try');
         await connection.send(Topics.INIT, token);
         alert('ws connection success');
-
       } catch (error) {
         alert(`ws connection error ${JSON.stringify(error)}`);
-
       }
     } catch (error) {}
   };
@@ -182,13 +94,16 @@ const MobileTradingView: FC = () => {
       });
   };
 
+  const callbackWidget = (tvWidget: IChartingLibraryWidget) => {
+    setTvWidget(tvWidget)
+  }
+
+
   const messageHandler = (e: MessageEvent) => {
-    alert(`message port1 ${JSON.stringify(e.data)}`);
+    alert(`message received ${JSON.stringify(e.data)}`);
     if (!activeSession) {
       Axios.defaults.headers['Authorization'] = e.data.auth;
-      initWebsocketConnection(e.data.auth).then(() => {
-        alert('websocket init done');
-        initWidget(activeSession!, e.data.instrument);
+      initWebsocketConnection(e.data.auth, e.data.instrument).then(() => {
       });
     }
 
@@ -217,7 +132,6 @@ const MobileTradingView: FC = () => {
   };
 
   useEffect(() => {
-    const { port1, port2 } = new MessageChannel();
 
     window.addEventListener('message', messageHandler, false);
 
@@ -248,7 +162,13 @@ const MobileTradingView: FC = () => {
   return (
     <FlexContainer height="100vh" width="100vw">
       <FlexContainer width="100%">
-        <FlexContainer width="100%" height="100%" id={containerId} />
+        {activeSession && (
+          <MobileChartContainer
+            activeSession={activeSession}
+            instrumentId={instrumentId}
+            callbackWidget={callbackWidget}
+          />
+        )}
       </FlexContainer>
     </FlexContainer>
   );
