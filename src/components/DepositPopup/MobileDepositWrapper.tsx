9a4544e19be92fbc8b4ab0634f6469678a269391
Shadymination <shadi.ganem@simplbit.com>
2020-04-29 10:27:44 +0300

    temp mobile wrapper

---
+++ src/components/DepositPopup/MobileDepositWrapper.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FlexContainer } from '../../styles/FlexContainer'
+
+const MobileDepositWrapper = () => {
+    return (
+        <FlexContainer width="100wv" height="100vh">
+            
+        </FlexContainer>
+    )
+}
+
+export default MobileDepositWrapper
