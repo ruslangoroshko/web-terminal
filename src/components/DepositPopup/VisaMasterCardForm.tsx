128e8746a257178f1583401c3ec818f01a392eb9
Shadymination <shadi.ganem@simplbit.com>
2020-04-30 14:33:05 +0300

    visacard form markup

--- src/components/DepositPopup/VisaMasterCardForm.tsx
+++ src/components/DepositPopup/VisaMasterCardForm.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import { FlexContainer } from '../../styles/FlexContainer';
-import { PrimaryTextParagraph } from '../../styles/TextsElements';
+import {
+  PrimaryTextParagraph,
+  PrimaryTextSpan,
+} from '../../styles/TextsElements';
 import styled from '@emotion/styled';
 import AmountPlaceholder from './AmountPlaceholder';
 import CurrencyDropdown from './CurrencyDropdown';
 import { paymentCurrencies } from '../../constants/paymentCurrencies';
+import Checkbox from '../Checkbox';
+import { Link } from 'react-router-dom';
+import { PrimaryButton } from '../../styles/Buttons';
 
 const VisaMasterCardForm = () => {
   const [amount, setAmount] = useState(1);
   const [currency, setCurrency] = useState(paymentCurrencies[0]);
+
+  const [imCardHolder, setImCardHolder] = useState(false);
 
   const placeholderValues = [50, 100, 250, 500, 1000];
 
@@ -30,6 +38,10 @@ const VisaMasterCardForm = () => {
 
   const handleChangeAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAmount(+e.target.value);
+  };
+
+  const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setImCardHolder(event.target.checked);
   };
 
   return (
@@ -69,6 +81,23 @@ const VisaMasterCardForm = () => {
           />
         ))}
       </GridDiv>
+      <FlexContainer marginBottom="24px">
+        <Checkbox
+          id="visa-master-card"
+          onChange={handleChangeCheckbox}
+          checked={imCardHolder}
+        >
+          <PrimaryTextSpan>I confirm that I am the cardholder.&nbsp;</PrimaryTextSpan>
+          <LearnMoreLink to="">Learn more</LearnMoreLink>
+        </Checkbox>
+      </FlexContainer>
+      <FlexContainer marginBottom="40px">
+        <PrimaryButton padding="12px 20px" width="180px">
+          <PrimaryTextSpan color="#003A38" fontSize="14px" fontWeight="bold">
+            Deposit {amount}
+          </PrimaryTextSpan>
+        </PrimaryButton>
+      </FlexContainer>
     </FlexContainer>
   );
 };
@@ -94,4 +123,11 @@ const Input = styled.input`
   padding: 12px;
   background-color: transparent;
   border-right: 1px solid rgba(255, 255, 255, 0.19);
+`;
+
+const LearnMoreLink = styled(Link)`
+  color: #fffccc;
+  &:hover {
+    color: #fffccc;
+  }
 `;
