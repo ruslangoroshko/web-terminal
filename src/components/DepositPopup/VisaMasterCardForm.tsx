78f5e0ed66873fc648234212219af3829b29c0b4
Ruslan Goroshko <rusgoroshko@gmail.com>
2020-07-01 14:35:52 +0300

    Fix encoding payment url

--- src/components/DepositPopup/VisaMasterCardForm.tsx
+++ src/components/DepositPopup/VisaMasterCardForm.tsx
@@ -65,7 +65,7 @@ const VisaMasterCardForm = () => {
     try {
       const response = await API.createDeposit(params);
       if (response.status === DepositApiResponseCodes.Success) {
-        window.open(encodeURIComponent(response.redirectUrl));
+        window.open(encodeURI(response.redirectUrl));
       } else {
         badRequestPopupStore.setMessage('Technical error');
         badRequestPopupStore.openModal();
