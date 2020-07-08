8f3ce7f6303dea6a9ebc549976e2ac891e3d1074
Ruslan Goroshko <rusgoroshko@gmail.com>
2020-07-08 14:15:38 +0300

    https://monfex.atlassian.net/browse/WEBT-306

--- src/components/SideBarTabs/PendingOrder.tsx
+++ src/components/SideBarTabs/PendingOrder.tsx
@@ -83,7 +83,7 @@ function PendingOrder(props: Props) {
             </PrimaryTextSpan>
           </FlexContainer>
           <PrimaryTextSpan fontSize="10px" color="rgba(255, 255, 255, 0.5)">
-            at {pendingOrder.openPrice.toFixed(2)}
+            at {pendingOrder.openPrice}
           </PrimaryTextSpan>
         </FlexContainer>
         <FlexContainer
