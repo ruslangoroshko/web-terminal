b7fe2904f3db7f018e77cdea74ca829701b587d4
Shadymination <shadi.ganem@simplbit.com>
2020-07-16 16:10:20 +0300

    added mixpanel events

---
+++ src/constants/mixpanelProps.ts
@@ -0,0 +1,27 @@
+const mixapanelProps = {
+  EMAIL: '$email',
+  BRAND_NAME: 'Brand Name',
+  ACCOUNTS: 'Accounts',
+  FUNDED_TRADER: 'Funded trader',
+  PHONE: '$phone',
+  TRADER_ID: 'Trader ID',
+  PLATFORMS_USED: 'Platforms Used',
+  FIRST_NAME: '$first_name',
+  KYC_STATUS: 'KYC Status',
+  DEPOSIT_METHOD: 'Deposit Method',
+  AMOUNT: 'Amount',
+  ACCOUNT_ID: 'Account id',
+  ACCOUNT_CURRENCY: 'Account currency',
+  INSTRUMENT_ID: 'Instrument ID',
+  MULTIPLIER: 'Multiplier',
+  TREND: 'Trend',
+  SLTP: 'SLTP',
+  AVAILABLE_BALANCE: 'Available Balance',
+  ACCOUNT_TYPE: 'Account Type',
+  EVENT_REF: 'Event ref',
+  ERROR_TEXT: 'Error text',
+};
+
+Object.freeze(mixapanelProps);
+
+export default mixapanelProps;
