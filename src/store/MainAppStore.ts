0063c17c6148cf1f3268d685787c03de1d86a2c7
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2020-09-14 14:10:58 +0300

    WEBT-398 Withdrawal available balance fixed

--- src/store/MainAppStore.ts
+++ src/store/MainAppStore.ts
@@ -99,6 +99,7 @@ export class MainAppStore implements MainAppStoreProps {
   connectTimeOut = '';
   @observable socketError = false;
   @observable activeAccountId: string = '';
+  @observable activeAccountAvailableBalance: number = 0;
   @observable signUpFlag: boolean = false;
 
   constructor(rootStore: RootStore) {
@@ -278,6 +279,7 @@ export class MainAppStore implements MainAppStoreProps {
         });
         this.activeAccount = activeAccount;
         this.activeAccountId = activeAccount.id;
+        this.activeAccountAvailableBalance = activeAccount.balance;
       } else {
         this.isDemoRealPopup = true;
       }
@@ -299,6 +301,7 @@ export class MainAppStore implements MainAppStoreProps {
   setActiveAccount = (account: AccountModelWebSocketDTO) => {
     this.activeAccount = account;
     this.activeAccountId = account.id;
+    this.activeAccountAvailableBalance = account.balance;
     // TODO: think how remove crutch
     this.rootStore.historyStore.positionsHistoryReport.positionsHistory = [];
     API.setKeyValue({
