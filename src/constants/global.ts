346cc425f0185a73a5237840d43caf028c71314d
Shadymination <shadi.ganem@simplbit.com>
2020-09-28 14:36:07 +0300

    lang problem

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,4 +1,4 @@
 export const LOCAL_STORAGE_TOKEN_KEY = 'current-token';
 export const LOCAL_STORAGE_TRADING_URL = 'trading-url';
 export const LOCAL_STORAGE_REFRESH_TOKEN_KEY = 'refresh-token';
-export const LOCAL_STORAGE_LANGUAGE = 'lang';
+export const LOCAL_STORAGE_LANGUAGE = 'language';
