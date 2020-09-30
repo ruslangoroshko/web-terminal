aabf743509977c0f59ace8e76af0f4838883c309
Shadymination <shadi.ganem@simplbit.com>
2020-09-30 15:10:05 +0300

    added check for dial

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -273,10 +273,12 @@ function SignUp() {
         const response = await API.getGeolocationInfo(
           mainAppStore.initModel.authUrl
         );
-        setDialMask(response.dial);
         const country = countries.find((item) => item.id === response.country);
         if (country) {
           setFieldValue(Fields.COUNTRY, country.name);
+        }
+        if (response.dial) {
+          setDialMask(`+${response.dial}`);
         }
       } catch (error) {}
     };
