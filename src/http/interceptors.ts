0f9b4d881495dd12cf67aeb6194bdfe0ef6f5e64
Shadymination <shadi.ganem@simplbit.com>
2020-11-09 17:52:36 +0200

    removed auth subpath rewrites

--- src/http/interceptors.ts
+++ src/http/interceptors.ts
@@ -61,21 +61,6 @@ const injectInerceptors = (mainAppStore: MainAppStore) => {
     }
   );
   axios.interceptors.request.use(function (config: AxiosRequestConfig) {
-    // TODO: sink about eat
-    if (
-      IS_LIVE &&
-      mainAppStore.initModel.tradingUrl &&
-      config.url &&
-      !config.url.includes('auth/')
-    ) {
-      if (config.url.includes('://')) {
-        const arrayOfSubpath = config.url.split('://')[1].split('/');
-        const subPath = arrayOfSubpath.slice(1).join('/');
-        config.url = `${mainAppStore.initModel.tradingUrl}/${subPath}`;
-      } else {
-        config.url = `${mainAppStore.initModel.tradingUrl}${config.url}`;
-      }
-    }
     config.headers[RequestHeaders.ACCEPT_LANGUAGE] = `${mainAppStore.lang}`;
     return config;
   });
