a66c444861a0fa3cce40950aeee06093b1adbc53
Shadymination <shadi.ganem@simplbit.com>
2020-11-17 11:10:16 +0200

    https://monfex.atlassian.net/browse/WEBT-654

--- src/constants/mixpanelValues.ts
+++ src/constants/mixpanelValues.ts
@@ -3,7 +3,7 @@ import { TpSlTypeEnum } from '../enums/TpSlTypeEnum';
 const mixpanelValues = {
   MOBILE_CHART: 'mobile chart',
   PORTFOLIO: 'portfolio',
-  CHART: 'CHART',
+  CHART: 'chart',
   [TpSlTypeEnum.Currency]: 'currency',
   [TpSlTypeEnum.Percent]: 'percent',
   [TpSlTypeEnum.Price]: 'price',
