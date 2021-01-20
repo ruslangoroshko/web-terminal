e9f16f6c02bffaef3421571c97d59b1161321ec2
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2021-01-20 14:16:24 +0200

    WEBT-869 Fix local for safari

--- src/store/MainAppStore.ts
+++ src/store/MainAppStore.ts
@@ -130,8 +130,8 @@ export class MainAppStore implements MainAppStoreProps {
     // @ts-ignore
     this.lang =
       localStorage.getItem(LOCAL_STORAGE_LANGUAGE) ||
-      ((navigator.language &&
-        polandLocalsList.includes(navigator.language))
+      ((window.navigator.language &&
+        polandLocalsList.includes(window.navigator.language.slice(0, 2).toLowerCase()))
         ? CountriesEnum.PL
         : CountriesEnum.EN);
     injectInerceptors(this);
