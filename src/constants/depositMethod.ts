963ef4bbb3df91b12cefb871a66299498972d970
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2021-03-17 13:25:17 +0200

    WEBT-1100 Fix mixpanel method

--- src/constants/depositMethod.ts
+++ src/constants/depositMethod.ts
@@ -2,7 +2,7 @@ const depositMethod = {
   BANK_CARD: 'bank card',
   BITCOIN: 'bitcoin',
   WIRE_TRANSFER: 'wire_transfer',
-  ELECTRONIC_FUNDS_TRANSFER: 'swiffy',
+  ELECTRONIC_FUNDS_TRANSFER: 'Swiffy',
 };
 
 Object.freeze(depositMethod);
