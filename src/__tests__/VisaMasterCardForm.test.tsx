8d88c3ab88d471d208c0f305864f29240601066b
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2021-07-07 18:49:54 +0300

    WEBT-1410 Change back card info

--- src/__tests__/VisaMasterCardForm.test.tsx
+++ src/__tests__/VisaMasterCardForm.test.tsx
@@ -14,7 +14,7 @@ import RequestHeaders from '../constants/headers';
 
 const API_DEPOSIT_STRING = process.env.TRADING_URL || 'http://localhost:5682';
 const AUTH_URL = process.env.API_AUTH_STRING || 'http://localhost:5679';
-const cardNumber = process.env.TEST_CARDNUMBER || '5555555555554444';
+const cardNumber = process.env.TEST_CARDNUMBER || '4242980372988546';
 const cvv = process.env.TEST_CARD_CVV || '333';
 const expirationDate = process.env.TEST_CARD_EXPIRE || '2024-09';
 const cardHolder = process.env.TEST_CARDHOLDER || 'Testing Name';
@@ -77,7 +77,7 @@ test('User can make a deposit with card, response status', async () => {
     cvv,
     expirationDate: new Date(expirationDate).getTime(),
     fullName: cardHolder,
-    amount: 500,
+    amount: 10,
     accountId: 'stl00000214usd',
   };
   const response = await createDepositInvoice(
