c477465a02c26ad529c456fd43214a39c5712c1a
Ruslan Goroshko <rusgoroshko@gmail.com>
2021-07-13 23:09:42 +0300

    add logger

--- src/http/interceptors.ts
+++ src/http/interceptors.ts
@@ -70,8 +70,11 @@ const injectInerceptors = (mainAppStore: MainAppStore) => {
     },
 
     async function (error) {
+      console.log('LOGGER');
       console.log('error url: ', error.response?.config?.url);
+      
       console.log('is ignored Debug: ', error.response?.config?.url.includes(API_LIST.DEBUG.POST));
+
       if (
         error.response?.config?.url.includes(API_LIST.DEBUG.POST) ||
         error.response?.config?.url.includes(API_LIST.ONBOARDING.STEPS)
