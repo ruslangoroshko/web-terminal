93bc249384dfd7c697f7e26fd5679ae4b2dc0190
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2021-10-13 18:04:38 +0300

    WEBT-1650 Try logging

--- src/components/EducationPopup.tsx
+++ src/components/EducationPopup.tsx
@@ -49,13 +49,17 @@ const EducationPopup: FC = observer(() => {
       (indexOfCourse !== null && indexOfCourse !== undefined) &&
       educationStore.coursesList
     ) {
+      console.log(indexOfCourse);
+      console.log(educationStore.coursesList);
       switch (indexOfCourse) {
         case 0:
           if (
             educationStore.coursesList[1]?.totalQuestions === educationStore.coursesList[1]?.totalQuestions
           ) {
+            console.log(0.1);
             educationStore.setActiveCourse(educationStore.coursesList[indexOfCourse + 2]);
           } else {
+            console.log(0.2);
             educationStore.setActiveCourse(educationStore.coursesList[indexOfCourse + 1]);
           }
           break;
@@ -63,8 +67,10 @@ const EducationPopup: FC = observer(() => {
           if (
             educationStore.coursesList[2]?.totalQuestions === educationStore.coursesList[2]?.totalQuestions
           ) {
+            console.log(1.1);
             educationStore.setActiveCourse(educationStore.coursesList[0]);
           } else {
+            console.log(1.2);
             educationStore.setActiveCourse(educationStore.coursesList[indexOfCourse + 1]);
           }
           break;
@@ -72,8 +78,10 @@ const EducationPopup: FC = observer(() => {
           if (
             educationStore.coursesList[0]?.totalQuestions === educationStore.coursesList[0]?.totalQuestions
           ) {
+            console.log(2.1);
             educationStore.setActiveCourse(educationStore.coursesList[1]);
           } else {
+            console.log(2.2);
             educationStore.setActiveCourse(educationStore.coursesList[0]);
           }
           break;
