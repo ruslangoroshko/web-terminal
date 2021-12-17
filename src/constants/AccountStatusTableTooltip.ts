d32c23fdbc3c6da8a5b457962d479f324f0d09a8
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2021-12-17 17:51:05 +0200

    WEBT-1782 Added hints texts

--- src/constants/AccountStatusTableTooltip.ts
+++ src/constants/AccountStatusTableTooltip.ts
@@ -9,43 +9,43 @@ export const AccountStatusTableTooltip: AccountStatusTableTooltipProps[]  = [
   {
     id: "deposit",
     label: "Deposit",
-    tooltipText: "test deposit",
+    tooltipText: "This is the amount of funds that will be available for you to trade with",
     blockWidth: "144px"
   },
   {
     id: "instruments",
     label: "Instruments",
-    tooltipText: "test",
+    tooltipText: "Number of Assets that can be traded",
     blockWidth: "144px"
   },
   {
     id: "personal_session",
     label: "Personal session",
-    tooltipText: "test",
+    tooltipText: "Type of included personal educational session with an investment manager",
     blockWidth: "172px"
   },
   {
     id: "webinars",
     label: "Webinars",
-    tooltipText: "test",
+    tooltipText: "Live sessions with a professional trader during market events or promotions",
     blockWidth: "144px"
   },
   {
     id: "analytics",
     label: "Analytics",
-    tooltipText: "test",
+    tooltipText: "Access to assisted trading software, indicators, analytics  and special charting tools",
     blockWidth: "144px"
   },
   {
     id: "spread",
     label: "Spread",
-    tooltipText: "test",
+    tooltipText: "A discount on the commission that occurs from the difference between Bid and Ask prices",
     blockWidth: "100px"
   },
   {
     id: "swap",
     label: "Swap",
-    tooltipText: "test",
+    tooltipText: "A discount on the over-the-night transactions",
     blockWidth: "100px"
   },
 ];
